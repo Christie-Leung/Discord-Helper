4b2125d9d4e3388eb1ba89c7cd08782dba90c323
Chelsea Chen <c.chen13@share.epsb.ca>
2021-03-22 14:40:24 -0600

    added web application

--- objects.py
+++ objects.py
@@ -4,10 +4,12 @@ import numpy
 
 
 class Button:
-    def __init__(self, X, Y, label):
+    def __init__(self, X, Y, label, W, H):
         self.X = X
         self.Y = Y
         self.LABEL = label
+        self.W = W
+        self.H = H
 
     def getX(self):
         return self.X
@@ -16,6 +18,12 @@ class Button:
 
     def getLabel(self):
         return self.LABEL
+
+    def getW(self):
+        return self.W
+
+    def getH(self):
+        return self.H
 
 
 
@@ -29,19 +37,28 @@ if __name__ == "__main__":
                 Y = list[1][1]
                 return X,Y
 
+
     driver = webdriver.Safari()
     driver.get("http://discord.com")
+    driver.maximize_window()
+
     # I am using Chrome Developer Tool to get the Xpath, you might want to use your own code.
     buttonDownload = driver.find_element_by_xpath('//*[@id="app-mount"]/div/div/div[1]/div[2]/div/div[2]/a')
     elementDownload = driver.find_element_by_xpath('//*[@id="app-mount"]/div/div/div[1]/div[2]/div/div[2]/a')
     labelDownload = elementDownload.text
     position = buttonDownload.location
+    dimensions = buttonDownload.size
     coor = list(position.items())
+    size = list(dimensions.items())
     Xdown,Ydown =getPosition(coor)
-    BUTTONDownloaded = Button(Xdown, Ydown, labelDownload)
+    Width, Height = getPosition(size)
+    BUTTONDownloaded = Button(Xdown, Ydown, labelDownload, Width, Height)
     print(BUTTONDownloaded.getLabel())
     print(BUTTONDownloaded.getX())
     print(BUTTONDownloaded.getY())
+    print(BUTTONDownloaded.getW())
+    print(BUTTONDownloaded.getH())
+
 
 
     #button open discord in your browser
@@ -50,20 +67,30 @@ if __name__ == "__main__":
     labelBrowser = elementBrowser.text
     positionBrowser = buttonBrowser.location
     coorBrowser = list(positionBrowser.items())
+    dimensionsBrowser = buttonBrowser.size
+    sizeBrowser = list(dimensionsBrowser.items())
+    WidthBrow, HeightBrow = getPosition(sizeBrowser)
     Xbrow, Ybrow = getPosition(coorBrowser)
-    BUTTONOFBROWSER = Button(Xbrow, Ybrow, labelBrowser)
+    BUTTONOFBROWSER = Button(Xbrow, Ybrow, labelBrowser, WidthBrow, HeightBrow)
     print(BUTTONOFBROWSER.getLabel())
     print(BUTTONOFBROWSER.getX())
     print(BUTTONOFBROWSER.getY())
+    print(BUTTONOFBROWSER.getW())
+    print(BUTTONOFBROWSER.getH())
 
     #login button
-    buttonLogin = driver.find_element_by_xpath('//*[@id="app-mount"]/div/div/div[1]/div[1]/header[1]/nav/div/a')
-    elementLogin = driver.find_element_by_xpath('//*[@id="app-mount"]/div/div/div[1]/div[1]/header[1]/nav/div/a')
+    buttonLogin = driver.find_element_by_xpath('//*[@id="app-mount"]/div/div/div[1]/div[1]/header[2]/nav/div[2]/a')
+    elementLogin = driver.find_element_by_xpath('//*[@id="app-mount"]/div/div/div[1]/div[1]/header[2]/nav/div[2]/a')
     labelLogin = elementLogin.text
     positionLogin = buttonLogin.location
     coorLogin = list(positionLogin.items())
     Xlog, Ylog = getPosition(coorLogin)
-    BUTTONOFLOGIN = Button(Xlog, Ylog, labelLogin)
+    dimensionsLogin = buttonLogin.size
+    sizeLogin = list(dimensionsLogin.items())
+    WidthLog, HeightLog = getPosition(sizeLogin)
+    BUTTONOFLOGIN = Button(Xlog, Ylog, labelLogin, WidthLog, HeightLog)
     print(BUTTONOFLOGIN.getLabel())
     print(BUTTONOFLOGIN.getX())
     print(BUTTONOFLOGIN.getY())
+    print(BUTTONOFLOGIN.getW())
+    print(BUTTONOFLOGIN.getH())
