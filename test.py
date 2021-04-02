8a4ee7baf98350598f2d275de31e166cb16d7d7c
Chelsea Chen <c.chen13@share.epsb.ca>
2021-04-02 01:41:56 -0600

    added web application

--- test.py
+++ test.py
@@ -1,18 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, request, url_for, redirect, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def page():
-    return render_template("design.html")
+def home():
+    return render_template("home.html")
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
+@app.route("/start", methods=['GET', 'POST'])
+def start():
+    if request.method == 'POST':
+        return redirect(url_for('start'))
 
 
 if __name__ == "__main__":
-    #urlStep = input("Please input url: ")
-    #if urlStep == "https://discord.com":
-        #app.run()
-    app.run()
+    app.run(debug = True)
