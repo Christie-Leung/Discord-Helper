4bd4d4cf5ec8c637bf4c13c5a42e398a2070b291
Chelsea Chen <c.chen13@share.epsb.ca>
2021-04-02 02:39:52 -0600

    fixed web application

--- test.py
+++ test.py
@@ -6,10 +6,37 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/start", methods=['GET', 'POST'])
+@app.route("/start")
 def start():
-    if request.method == 'POST':
-        return redirect(url_for('start'))
+    return render_template('design.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+@app.route("/join")
+def join():
+    return render_template('join.html')
+
+@app.route("/joinServer")
+def joinServer():
+    return render_template('joinServer.html')
+
+@app.route("/server")
+def server():
+    return render_template('server.html')
+
+@app.route("/verification")
+def verification():
+    return render_template('verification.html')
+
+@app.route("/confirmation")
+def confirmation():
+    return render_template('confirmation.html')
 
 
 if __name__ == "__main__":
